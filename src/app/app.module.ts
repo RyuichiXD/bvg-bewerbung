a5a9172e9770fc7ecc8108c41ebb482d8e40f088
RyuichiXD <samed.sulanc@yahoo.de>
2020-04-21 18:45:06 +0200

    added a ngbootstrap table with pagination, custom style and dummy-messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+
+//import { NgbdTablePagination } from './table-pagination';
+//custom made
 import { TrafficMessagesListComponent } from './traffic-messages-list/traffic-messages-list.component';
+import { TrafficMessagesFilterComponent } from './traffic-messages-filter/traffic-messages-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrafficMessagesListComponent
+    TrafficMessagesListComponent,
+    TrafficMessagesFilterComponent
   ],
   imports: [
+    FormsModule,
+    NgbModule,
     BrowserModule
   ],
   providers: [],
--- src/app/traffic-messages-list/traffic-messages-list.component.ts
+++ src/app/traffic-messages-list/traffic-messages-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DUMMY_MESSAGES } from './dummy-data';
 
 @Component({
   selector: 'traffic-messages-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./traffic-messages-list.component.scss']
 })
 export class TrafficMessagesListComponent implements OnInit {
+  
+  private selectedPage: number = 1;
+  private pageSize: number = 10;
+  private collectionSize = DUMMY_MESSAGES.length;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+ get tableElements() {
+    return DUMMY_MESSAGES
+      .map((country, i) => ({id: i + 1, ...country}))
+      .slice((this.selectedPage - 1) * this.pageSize, (this.selectedPage - 1) * this.pageSize + this.pageSize);
+  } 
 }
