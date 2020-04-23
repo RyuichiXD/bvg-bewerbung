b8eed0ee6824d1727ea2ba65e5394caa592847ea
RyuichiXD <samed.sulanc@yahoo.de>
2020-04-23 13:42:18 +0200

    added modal view for showing the details of the api-response. Select a entry from the table to open a modal

---
+++ src/app/modal-message-details/modal-message-details.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input  } from '@angular/core';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'; // to open related messages 
+import { Message } from "./../traffic-messages-list/schemas.model";
+
+@Component({
+  selector: 'modal-message-details',
+  templateUrl: './modal-message-details.component.html',
+  styleUrls: ['./modal-message-details.component.scss']
+})
+export class ModalMessageDetailsComponent implements OnInit {
+
+  @Input() messages : Message[];
+  @Input() index :number;
+
+  //googlemaps paramter
+  lat: number;
+  lng: number;
+  zoom: number = 15;
+
+  constructor(public modalService: NgbModal, public activeModal: NgbActiveModal) { }
+
+  ngOnInit() {
+    this.lat = this.messages[this.index].geokoordinaten.latitude;
+    this.lng = this.messages[this.index].geokoordinaten.longitude;
+  }
+
+ openModal(id) {
+    const index = this.messages.findIndex(element => id === element.meldungsId);
+    if(index !== -1)
+    {    
+      const modalRef = this.modalService.open(ModalMessageDetailsComponent,{ centered: true, size: 'lg' });
+      modalRef.componentInstance.messages = this.messages;
+      modalRef.componentInstance.index = index;
+    }
+  } 
+}
